bea48131397e1d0aeb14292131dd2c81879a337c
José Moreno <59598804+juanjosemorenom@users.noreply.github.com>
2025-09-27 13:51:21 -0600

    Ejemplo de Listas con For

---
+++ ejemploListasFor.py
@@ -0,0 +1,9 @@
+# Generar una lista de cuadrados a partir de otra lista
+numeros = [1, 2, 3, 4, 5]
+cuadrados = []
+
+for n in numeros:
+    cuadrados.append(n ** 2)
+
+print("Lista original:", numeros)
+print("Lista de cuadrados:", cuadrados)
